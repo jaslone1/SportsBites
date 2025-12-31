4cf1fb5853dccdfd1c610f497d913e095c3f3687
jaslone <jaredmslone@gmail.com>
2025-12-31 10:18:16 -0600

    Added in standalone line for register and added to imports

--- SportsBitesUI/src/app/app-routing-module.ts
+++ SportsBitesUI/src/app/app-routing-module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { EventListComponent } from './components/event-list/event-list.component';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
--- SportsBitesUI/src/app/components/register/register.component.ts
+++ SportsBitesUI/src/app/components/register/register.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
+  standalone: false,
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
