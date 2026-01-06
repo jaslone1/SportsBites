ce66826f1aeca58364fd356a8c1cb72ff70eec0b
jaslone <jaredmslone@gmail.com>
2026-01-06 15:09:01 -0600

    adding error message to chase edit issue

--- SportsBitesUI/src/app/components/event-details/event-details.component.ts
+++ SportsBitesUI/src/app/components/event-details/event-details.component.ts
@@ -147,6 +147,12 @@ export class EventDetailsComponent implements OnInit {
     const isHost = this.isHost();
 
     return isSuggester || isHost;
+    console.log('EDIT FOOD CHECK', {
+      foodId: food.foodSuggestionId,
+      suggestedByUserId: food.suggestedByUserId,
+      currentUserId: userId,
+      isHost: this.isHost()
+    });
   }
 
   // Check if current user is the one who claimed the item
